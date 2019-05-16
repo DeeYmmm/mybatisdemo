52d2fbc7625e0edfab9a356b38cd33f51b09fc2a
DeeYmmm <593087509@qq.com>
2019-05-16 20:15:03 +0800

    mybatis缓存机制

--- src/test/java/MybatisTest5.java
+++ src/test/java/MybatisTest5.java
@@ -32,11 +32,38 @@ public class MybatisTest5 {
 
             //2.SqlSession相同，手动清除了缓存(只清除当前SqlSession相同的一级缓存)
             //sqlSession.clearCache();
+
+            //3.SqlSession相同,两次查询之间进行了增删改操作
+            /*Person person2=new Person("塞拉斯","男");
+            mapper.insertPerson(person2);
+            sqlSession.commit();*/
             Person person1 = mapper.getPersonById(40);
             System.out.println(person1);
             System.out.println(person==person1);
         } finally {
             sqlSession.close();
+        }
+    }
+
+    @Test
+    public void test2() throws IOException {
+        SqlSessionFactory sqlSessionFactory = getSqlSessionFactory();
+        SqlSession sqlSession = sqlSessionFactory.openSession();
+        SqlSession sqlSession1 = sqlSessionFactory.openSession();
+        try {
+            PersonMapper mapper = sqlSession.getMapper(PersonMapper.class);
+            PersonMapper mapper1 = sqlSession1.getMapper(PersonMapper.class);
+
+            Person person = mapper.getPersonById(40);
+            System.out.println(person);
+            sqlSession.close();
+
+            Person person2=new Person("Lux","nv");
+            mapper1.insertPerson(person2);
+            Person person1 = mapper1.getPersonById(40);
+            System.out.println(person1);
+        } finally {
+            sqlSession1.close();
         }
     }
 }
